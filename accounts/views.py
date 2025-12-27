94aa3ee9b63a6129f74003a1a9d6079c4601473d
Mahdi.Fazeli.1997 <fazelimahdi76@gmail.com>
2025-12-27 17:19:00 +0330

    show messages

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def user_register(request):
     if request.method == 'POST':
@@ -11,6 +12,7 @@ def user_register(request):
             User.objects.create_user(username=data['username'], email=data['email'],
                                      first_name=data['first_name'],last_name=data['last_name'],
                                      password=data['password_2'])
+            messages.success(request, f'Account created for {data["username"]}',extra_tags="primary")
             return redirect('home:home')
         return render(request,'accounts/register.html',{'form': form})
 
@@ -30,9 +32,12 @@ def user_login(request):
                 user = authenticate(username=data['user'], password=data['password'])
                 if user is not None:
                     login(request, user)
+                    messages.success(request, 'You are now logged in',extra_tags="success")
                     return redirect('home:home')
-        else:
-            return render(request, 'accounts/login.html', {'form': form})
+                else:
+                    form = UserLoginForm()
+                    messages.error(request, 'Invalid username or password',extra_tags="danger")
+                    return render(request, 'accounts/login.html', {'form': form})
 
     else:
         form = UserLoginForm()
@@ -40,4 +45,5 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
+    messages.success(request, 'You are now logged out',extra_tags="warning")
     return redirect('accounts:user_login')
